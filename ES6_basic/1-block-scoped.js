ee40b0a49631a0e73ac438e5fb11248bd211099a
malak33s <melaksarhani1bga@gmail.com>
2024-11-13 04:27:10 +0100

    parameters defaults

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    let task = false;  // la j'utilise de let pour que task soit limitée à la fonction
+    let task2 = true;   // ensuite encore let task2 soit limitée à la fonction
   
     if (trueOrFalse) {
-      task = true;
-      task2 = false;
+      task = true;      // Pas besoin de var ici, juste affectation
+      task2 = false;    // la meme chose
     }
   
     return [task, task2];
