b538a494f25c8d41cd0297a076253cdc82c57ebb
malak33s <melaksarhani1bga@gmail.com>
2024-11-17 00:10:03 +0100

    correction task one '

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // j'utilise let pour que task soit limitée à la fonction
-  let task2 = true; // encore let pour que task2 soit limitée à la fonction
+  let task = false; // Use let to ensure block scope
+  let task2 = true; // Use let to ensure block scope
 
   if (trueOrFalse) {
-    task = true; // Pas besoin de var ici, juste une affectation
-    task2 = false; // pareil ici
+    let task = true; // This variable is scoped to the block
+    let task2 = false; // This variable is scoped to the block
   }
 
-  return [task, task2];
+  return [task, task2]; // Returns the outer scope variables
 }
