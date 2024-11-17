e7e8477c31bbd87fb10612a41646f448fd0bb099
malak33s <melaksarhani1bga@gmail.com>
2024-11-17 01:26:04 +0100

    execution

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
-/*  no-unused-vars */
-/*  no-var */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; // Utilisation de const car cette valeur ne change pas
+  const task2 = true; // Idem ici
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // Variable distincte avec une portée limitée au bloc
+    const task2 = false; // Variable distincte avec une portée limitée au bloc
   }
 
-  return [task, task2];
+  return [task, task2]; // Les valeurs globales (false, true) sont retournées
 }
