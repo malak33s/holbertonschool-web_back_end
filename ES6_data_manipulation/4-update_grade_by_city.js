e36ef44bce024e262a81836cda52dc466a9e04c1
malak33s <melaksarhani1bga@gmail.com>
2024-11-21 17:24:28 +0100

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+    if (!Array.isArray(list)) return [];
+  
+    return list
+      .filter((student) => student.location === city)
+      .map((student) => {
+        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+        return {
+          ...student,
+          grade: gradeObj ? gradeObj.grade : 'N/A',
+        };
+      });
+  }
+  
