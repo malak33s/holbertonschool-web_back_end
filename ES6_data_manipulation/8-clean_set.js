6a21c084c6e1cb05666f6496b3f234028e9fc6f2
malak33s <melaksarhani1bga@gmail.com>
2024-11-22 14:12:00 +0100

    correction indentation

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,8 @@
 export default function cleanSet(set, startString) {
-    if (!startString || typeof startString !== 'string') return '';
-  
-    return [...set]
-      .filter((value) => value.startsWith(startString))
-      .map((value) => value.slice(startString.length))
-      .join('-');
-  }
-  
+  if (!startString || typeof startString !== 'string') return '';
+
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
