0c5a9c181570b566af6ecd11c1cb3a1c8a4d463a
malak33s <melaksarhani1bga@gmail.com>
2024-11-22 16:24:51 +0100

    correction indentation all fill

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,3 @@
 export default function getStudentIdsSum(list) {
-    return list.reduce((sum, student) => sum + student.id, 0);
-  }
-  
+  return list.reduce((sum, student) => sum + student.id, 0);
+}
--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,13 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-    if (!Array.isArray(list)) return [];
-  
-    return list
-      .filter((student) => student.location === city)
-      .map((student) => {
-        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-        return {
-          ...student,
-          grade: gradeObj ? gradeObj.grade : 'N/A',
-        };
-      });
-  }
-  
+  if (!Array.isArray(list)) return [];
+
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
