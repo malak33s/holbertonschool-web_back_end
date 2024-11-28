3dd2f8bb7c085cf26b4ecb36c5599fadc70ef71d
malak33s <melaksarhani1bga@gmail.com>
2024-11-28 16:13:07 +0100

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,19 @@
-function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.race([firstName, lastName, fileName]);
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // deux fonctions arguments appropriés
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  // pour gérer les deux promesses
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => {
+      return results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return { status: 'fulfilled', value: result.value };
+        }
+        return { status: 'rejected', reason: result.reason };
+      });
+    });
 }
-module.exports = handleProfileSignup;
