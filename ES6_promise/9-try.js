de0f6dd6f67dfd068a97f0c4ea4ea954cef13833
malak33s <melaksarhani1bga@gmail.com>
2024-11-28 16:21:58 +0100

    task nine

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+      const result = mathFunction(); // Execute the math function
+      queue.push(result); // If no error, push the result
+    } catch (error) {
+      queue.push(error.message); // If an error is thrown, push the error message
+    } finally {
+      queue.push('Guardrail was processed'); // Always push this message
+    }
+    return queue;
+  }
