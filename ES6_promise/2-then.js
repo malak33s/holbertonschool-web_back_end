99d5622d14cd163f529ada0a3a0b75560f4d661d
malak33s <melaksarhani1bga@gmail.com>
2024-11-28 12:22:16 +0100

    task 2 correction

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,11 @@
+// 2-then.js
 function handleResponseFromAPI(promise) {
-    return promise
-      .then(() => {
-        console.log("Got a response from the API");
-        return { status: 200, body: "success" };
-      })
-      .catch(() => {
-        return new Error();
-      });
-  }
-  
-  export default handleResponseFromAPI;
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
+}
+
+export default handleResponseFromAPI;
