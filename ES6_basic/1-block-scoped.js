18ddb663f9416d1188a1abc0a21d1b2ff9eb589b
malak33s <melaksarhani1bga@gmail.com>
2024-11-29 22:46:22 +0100

    trop de com

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
-  const task = false; // Variable déclarée avec const, portée limitée à la fonction
-  const task2 = true; // Idem ici
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
 
-  if (trueOrFalse) {
-    const task = true; // Nouvelle variable, portée limitée au bloc 'if'
-    const task2 = false; // Nouvelle variable, portée limitée au bloc 'if'
-  }
-
-  return [task, task2]; // Retourne les variables définies en dehors du bloc 'if'
+  return [task, task2];
 }
