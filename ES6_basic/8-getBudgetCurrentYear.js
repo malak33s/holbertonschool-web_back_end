b6258bf2b36594f91d55e8216d3a08c933bdfe87
malak33s <melaksarhani1bga@gmail.com>
2024-11-29 22:39:26 +0100

    task 8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,12 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const currentYear = getCurrentYear();
-  
-    return {
-      [`income-${currentYear}`]: income,
-      [`gdp-${currentYear}`]: gdp,
-      [`capita-${currentYear}`]: capita,
-    };
-  }
-  
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
