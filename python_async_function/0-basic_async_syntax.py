64f12402052dae639af0d663b896f93baeb78d84
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 15:21:00 +0100

    task zero

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,5 @@ import asyncio
 async def wait_random(max_delay: int = 10) -> float:
     """Returns a random delay between 0 and max_delay (inclusive)."""
     delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay) """ Simulation de l'attente
-    """
+    await asyncio.sleep(delay)
     return delay
