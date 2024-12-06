5a8eead75eb20bafb76f13e2ed615be55926694b
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 14:40:47 +0100

    pycode style

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-# une fonction qui crée une fonction pour multiplier un nombre par un facteur donné
-
+''' fonction crée fonction pour multiplier un nombre par un facteur donné
+'''
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    # on retourne une fonction qui multiplie un nombre par le facteur
+    ''' on retourne une fonction qui multiplie un nombre par le facteur
+    '''
     return lambda x: x * multiplier
