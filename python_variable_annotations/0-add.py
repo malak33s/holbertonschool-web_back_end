6cdeb1baaf48bf038a50c9cf21f2e417fa09dca6
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 13:38:54 +0100

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# une fonction qui ajoute deux nombres décimaux et retourne la somme
+''' une fonction qui ajoute deux nombres décimaux et retourne la somme '''
+
 
 def add(a: float, b: float) -> float:
     # on retourne la somme des deux nombres
