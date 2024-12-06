ae16c64b988ffda8ca817a411a9906406a0685c2
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 14:14:52 +0100

    pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-# une fonction qui retourne un tuple avec une chaîne de caractères et un nombre au carré
-
+''' une fonction qui retourne un tuple avec une chaîne de caractères et un nombre au carré
+'''
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    # on retourne un tuple avec la chaîne et le carré du nombre
+    ''' on retourne un tuple avec la chaîne et le carré du nombre
+    '''
     return (k, float(v ** 2))
