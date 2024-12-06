be61893e69614cdee041a28b0af9cecea9531255
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 13:56:29 +0100

    task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
-# une fonction qui transforme un nombre décimal en texte
+"""
+Ce module contient une fonction qui retourne la représentation sous forme
+de chaîne de caractères d'un nombre flottant.
+"""
+
 
 def to_str(n: float) -> str:
-    # on retourne le nombre sous forme de texte
+    """
+    Retourne la représentation en chaîne de caractères d'un nombre flottant.
+
+    Args:
+        n (float): Le nombre flottant.
+
+    Returns:
+        str: La représentation en chaîne du nombre flottant.
+    """
     return str(n)
