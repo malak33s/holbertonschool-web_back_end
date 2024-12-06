4c97c847dd54ee294322e2fbde37d9f02e296173
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 11:32:09 +0100

    task one

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,5 +2,5 @@
 # une fonction qui colle deux chaînes de caractères ensemble
 
 def concat(str1: str, str2: str) -> str:
-    # on retourne la combinaison des deux chaînes
+    # return combinaison des deux chaînes
     return str1 + str2
