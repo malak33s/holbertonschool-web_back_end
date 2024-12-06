74079eb914ea160a628601ac82555e7c22103ce7
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 11:18:29 +0100

    tasl 0 and task 2

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# une fonction qui ajoute deux nombres décimaux et retourne la somme
+
+def add(a: float, b: float) -> float:
+    # on retourne la somme des deux nombres
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# une fonction qui colle deux chaînes de caractères ensemble
+
+def concat(str1: str, str2: str) -> str:
+    # on retourne la combinaison des deux chaînes
+    return str1 + str2
