c503cf3b8c5dc0f227bbeebcac7fcdd3fa6a8e6e
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 15:55:12 +0100

    task one not complete

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 import asyncio
-from typing import List
-from 0-basic_async_syntax import wait_random
+from basic_async_syntax import wait_random
 
-''' cette fonction exécute wait_random n fois et renvoie les délais dans l'ordre croissant '''
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    ''' on crée une liste avec n délais, chacun généré par wait_random '''
-    delays = [await wait_random(max_delay) for _ in range(n)]
-    ''' on renvoie les délais triés sans utiliser sort() (ça se fait naturellement avec l'asynchronicité) '''
+"""
+ce module contient une fonction asynchrone qui exécute wait_random n fois et retourne 
+les délais dans l'ordre croissant
+"""
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+    exécute wait_random n fois avec un délai maximum donné et renvoie les résultats
+    dans l'ordre croissant (sans utiliser la fonction sorted())
+
+    Args:
+    n (int): le nombre de fois à appeler wait_random
+    max_delay (int): délai maximum (inclus) en secondes
+
+    Returns:
+    list: une liste de délais générés par wait_random dans l'ordre croissant
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
     return sorted(delays)
