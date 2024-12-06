a5d717fb96dcb6f43f489020d0a1b8a28c11e873
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 14:06:16 +0100

    task pycoestyle

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-# une fonction qui additionne une liste de nombres décimaux
-
+'''une fonction qui additionne une liste de nombres décimaux
+'''
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     # on retourne la somme de tous les nombres dans la liste
