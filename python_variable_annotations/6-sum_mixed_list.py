5722e943c332c6cb4c1eec96352c5715d035344d
malak33s <melaksarhani1bga@gmail.com>
2024-12-06 14:09:52 +0100

    task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-# une fonction qui additionne une liste de nombres entiers et décimaux
-
+""" une fonction qui additionne une liste de nombres entiers et décimaux
+"""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    # on retourne la somme de tous les nombres dans la liste
+    """ on retourne la somme de tous les nombres dans la liste
+    """
     return sum(mxd_lst)
