6e444df0a695b4b5d696b941067271d6fad913b0
malak33s <melaksarhani1bga@gmail.com>
2024-12-10 11:17:09 +0100

    coroutine with asyngenerator qui prend aucun argu

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,9 +7,10 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    ça va génerer 10 nombres aléatoires entre 0 et 10, avec pause de 1 seconde entre chaque.
+    génere 10 nbrs random entre 0 et 10, avc pause de 1 seconde entre chaque.
     """
     for _ in range(10):
         await asyncio.sleep(1)
