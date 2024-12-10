3d1775456d2dc3f8800dfda486ea15b97a4626a1
malak33s <melaksarhani1bga@gmail.com>
2024-12-10 15:42:36 +0100

    changement du import et du nom du fichier car doit pas commencer par un chiffre

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-Module pour collecter des nombres aléatoires générés de manière asynchrone.
+Module pour collecter des nombres aléatoires générés
+de manière asynchrone.
 """
 
+from async_generator import async_generator
 from typing import List
-from 0_async_generator import async_generator
 
 
 async def async_comprehension() -> List[float]:
@@ -12,5 +13,4 @@ async def async_comprehension() -> List[float]:
     Utilise une compréhension asynchrone pour collecter
     10 nombres générés par async_generator.
     """
-    numbers = [n async for n in async_generator()]
-    return numbers
+    return [n async for n in async_generator()]
--- python_async_comprehension/1-main.py
+++ python_async_comprehension/1-main.py
@@ -6,7 +6,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def main():
-    
     print(await async_comprehension())
 
 asyncio.run(main())
