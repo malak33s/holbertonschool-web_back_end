c3c3f8bf6a7e2c76b0c47858a20ba5b7940296bd
malak33s <melaksarhani1bga@gmail.com>
2024-12-10 14:29:18 +0100

    coroutine with asyngenerator qui prend aucun argu

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module contient une coroutine pour générer des nombres random.
+Ce module contient une coroutine pour générer des nombres aléatoires.
 """
 
 import asyncio
@@ -10,7 +10,7 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    génere 10 nbrs random entre 0 et 10, avc pause de 1 seconde entre chaque.
+    Génèrate 10 random numbers btwn 0/10 with pose de 1 seconde entre chaque.
     """
     for _ in range(10):
         await asyncio.sleep(1)
