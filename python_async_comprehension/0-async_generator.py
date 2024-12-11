d4cb11adb9990f31e2fd1999a3029374dd3ee671
malak33s <melaksarhani1bga@gmail.com>
2024-12-11 14:13:26 +0100

    changin import generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,14 +5,14 @@ Ce module contient une coroutine pour générer des nombres aléatoires.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 """
 import random and generate
 """
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Génèrate 10 random numbers btwn 0/10 with pose de 1 seconde entre chaque.
     """
