123957d4c58f2faa738443ddeec5f3b16d8bc795
malak33s <melaksarhani1bga@gmail.com>
2024-12-11 14:01:15 +0100

    task 0 pycode style

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,9 +7,11 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
-""" 
+"""
 import random and generate
 """
+
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Génèrate 10 random numbers btwn 0/10 with pose de 1 seconde entre chaque.
