3aa4c31dce2958e74555ec49ffae42b362c0fd43
malak33s <melaksarhani1bga@gmail.com>
2025-01-20 13:25:28 +0100

    eslint correction espace ligne

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // affiche un mess
-function displayMessage(message){
-    console.log(message);
+function displayMessage(message) {
+  console.log(message);
 }
 // deplacer la fonction pr utilisation ailleurs
 module.exports = displayMessage;
